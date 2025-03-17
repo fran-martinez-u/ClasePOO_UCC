9c7f318c2bad88e3a5a2b546a7be7ca15ae26da6
fran-martinez-u <fran.martinez@campusucc.edu.co>
2025-03-17 15:12:45 +0000

    Cambio de nombre

--- Clase 7/Main.java
+++ Clase 7/Main.java
@@ -1,9 +1,19 @@
-public class Ejecutar {
+public class Main {
 
     public static void main (String[] args) {
 
         //Instanciar el objeto (creaciiiion)
         Estudiante objEst1 = new Estudiante(1, "Gabriela", "Astorquiza", 15, "Ingenieria");
         Estudiante objEst2 = new Estudiante(2, "Milanesa", "Puberta", 48, "Ingenieria");
+
+        System.out.println(objEst1);
+        System.out.println(objEst2);
+
+        //Modificar edad
+        objEst1.setEdad(120);
+        
+
+        System.out.println(objEst1);
+      
     }
 }
