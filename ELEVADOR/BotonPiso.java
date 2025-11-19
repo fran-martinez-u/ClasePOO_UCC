16fb57fc57a34cbdde3a89431b86c7f4c4563b30
fran-martinez-u <fran.martinez@campusucc.edu.co>
2025-11-19 17:12:34 -0500

    Crear BotonPiso.java

---
+++ ELEVADOR/BotonPiso.java
@@ -0,0 +1,15 @@
+public class BotonPiso extends Boton {
+
+    public BotonPiso(Ascensor ascensor, int pisoDestino) {
+        super(ascensor, pisoDestino);
+    }
+
+    @Override
+    public void presionar() {
+        if (activo) {
+            System.out.println("Botón en el PISO presionado - Piso " + pisoDestino);
+            ascensor.solicitarPiso(pisoDestino);
+            iluminar();
+        }
+    }
+}
