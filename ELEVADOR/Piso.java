80ba4278499e8b340aed375af3e8faf86919b787
fran-martinez-u <fran.martinez@campusucc.edu.co>
2025-11-19 23:49:05 -0500

    1.2

--- ELEVADOR/Piso.java
+++ ELEVADOR/Piso.java
@@ -1,24 +1,57 @@
 package ELEVADOR;
 
-public class BotonPiso extends Boton {
-    private String direccion;
+public class Piso {
+    private int numero;
+    private Boton botonSubir;
+    private Boton botonBajar;
+    private Ascensor ascensor;
 
-    public BotonPiso(String direccion) {
-        this.direccion = direccion;
+    public Piso(int numero, int totalPisos, Ascensor ascensor) {
+        this.numero = numero;
+        this.ascensor = ascensor;
+
+        if (numero != totalPisos) {
+            this.botonSubir = new BotonPiso("subir");
+        }
+        if (numero != 1) {
+            this.botonBajar = new BotonPiso("bajar");
+        }
+    }
+    // Solicita subir 
+    public void solicitarSubir() {
+        if (botonSubir != null) {
+            botonSubir.solicitar(); //
+            ascensor.solicitarPiso(numero);
+        } else {
+            System.out.println("No se puede SUBIR desde el piso " + numero + " (último piso)");
+        }
+    }
+    // Solicita bajar
+    public void solicitarBajar() {
+        if (botonBajar != null) {
+            botonBajar.solicitar();
+            ascensor.solicitarPiso(numero);
+        } else {
+            System.out.println("No se puede BAJAR desde el piso " + numero + " (primer piso)");
+        }
     }
 
-    @Override
-    public void solicitar() {
-        System.out.println("Solicitud de " + direccion.toUpperCase() + " desde pasillo");
-        presionar();
+    public void ascensorLlego() {
+        if (botonSubir != null)
+            botonSubir.apagar();
+        if (botonBajar != null)
+            botonBajar.apagar();
+    }
+    // Getters
+    public int getNumero() {
+        return numero;
     }
 
-    public String getDireccion() {
-        return direccion;
+    public Boton getBotonSubir() {
+        return botonSubir;
     }
 
-    @Override
-    public String toString() {
-        return "BotonPiso [direccion=" + direccion + ", iluminado=" + iluminado + "]";
+    public Boton getBotonBajar() {
+        return botonBajar;
     }
 }
